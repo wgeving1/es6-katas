735f2fbdf335739287eb026a1b6e6671fcec2049
William Geving <wgeving@xmission.com>
2019-03-30 10:10:43 -0600

    Rest, Iterator, Generator, Modules and Reflect

--- 61-modulesImport.js
+++ 61-modulesImport.js
@@ -2,7 +2,9 @@
 // To do: make all tests pass, leave the assert lines unchanged!
 
 import assert from 'assert'; // is only here for completeness, `assert` is always imported by default
+import { equal, equal as myEqual, deepEqual, notEqual } from 'assert';
 
+import myAssert from 'assert';
 
 describe('use `import` to import functions that have been exported (somewhere else)', function() {
   describe('the import statement', function() {
@@ -17,27 +19,22 @@ describe('use `import` to import functions that have been exported (somewhere el
   });
   describe('import members', function() {
     it('import a single member, using `import {<memberName>} from "module"`', function() {
-      const equal = assert.equal;
       assert.strictEqual(equal, assert.equal);
     });
     describe('separate multiple members with a comma', function() {
       it('`deepEqual` from the assert module', () => {
-        const deepEqual = assert.deepEqual;
         assert.strictEqual(deepEqual, assert.deepEqual);
       });
       it('`notEqual` from the assert module', () => {
-        const notEqual = assert.notEqual;
         assert.strictEqual(notEqual, assert.notEqual);
       });
     });
   });
   describe('alias imports', function() {
     it('using `member as alias` as memberName', function() {
-      const myEqual = assert.equal;
       assert.strictEqual(myEqual, assert.equal);
     });
     it('rename the default export of a module, using `default as alias` as memberName', function() {
-      const myAssert = assert;
       assert.strictEqual(myAssert, assert);
     });
   });
